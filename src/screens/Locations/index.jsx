3943dc8e7cac41ebd8a11aab1f0fd2d9fcab64b4
gunayakay <101050062+gunayakay@users.noreply.github.com>
2022-09-05 04:08:02 +0300

    Locations is done.

--- src/screens/Locations/index.jsx
+++ src/screens/Locations/index.jsx
@@ -2,12 +2,12 @@ import { FlatList, Text } from "react-native";
 import React from "react";
 import { Box } from "native-base";
 import { useQuery } from "@tanstack/react-query";
+import getLocation from "../../services/getLocations";
 import Locationbox from "./components/Locationbox";
-import getLocation from "../../services/getLocation";
 import Loading from "../../components/loading";
 
 function Abilities() {
-  const { data, isLoading, isError } = useQuery(["getAbilities"], getAbilities);
+  const { data, isLoading, isError } = useQuery(["getLocation"], getLocation);
   if (isLoading) return <Loading />; // useState
   if (isError) return <Text>Error</Text>; // useMemo
 
